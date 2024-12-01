bf253cce90926c4ca10e3109f9dedc7ca9bdde99
bharathkuppala <bharath.kuppala@simplifyvms.com>
2024-12-01 22:12:19 +0530

    terraform outputs

--- 03-variables/main.tf
+++ 03-variables/main.tf
@@ -2,10 +2,30 @@ variable "x" {
   default = 10
 }
 
+variable "y" {
+  default = true
+}
+
+variable "z" {
+  default = "helloworld"
+}
+
+# list variable
+variable "l" {
+  default = [1,'ad',false,21.1]
+}
+
+
+
 output "x" {
   value = var.x
 }
 
 output "x1" {
   value = var.x
+}
+
+output "x2" {
+  value = "value of y - ${var.y}  ${var.z}"
+
 }
