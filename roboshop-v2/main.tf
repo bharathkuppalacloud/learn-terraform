be45cb8f5739e5499485b19062d9fc30af0dc5de
bharathkuppala <bharath.kuppala@simplifyvms.com>
2024-12-02 17:34:31 +0530

    added mongo and catalogue

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -33,3 +33,43 @@ resource "aws_route53_record" "frontend" {
   ttl     = 15
   records = [aws_instance.frontend.private_ip]
 }
+
+
+resource "aws_instance" "mongo" {
+  ami           = var.ami-image
+  instance_type = var.instance_type
+  vpc_security_group_ids=[var.security_group_id]
+
+  tags = {
+    Name = "mongo.dev"
+  }
+}
+
+resource "aws_route53_record" "mongo" {
+  zone_id = var.zone
+  name    = "mongo.dev.codedeploywithbharath.tech"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.mongo.private_ip]
+}
+
+
+resource "aws_instance" "catalogue" {
+  ami           = var.ami-image
+  instance_type = var.instance_type
+  vpc_security_group_ids=[var.security_group_id]
+
+  tags = {
+    Name = "catalogue.dev"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = var.zone
+  name    = "catalogue.dev.codedeploywithbharath.tech"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.catalogue.private_ip]
+}
+
+
