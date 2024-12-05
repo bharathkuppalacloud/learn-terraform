c5cc00c895513a1b317b3ac74bd288f640a5bd52
bharathkuppala <bharath.kuppala@simplifyvms.com>
2024-12-05 18:51:35 +0530

    modules

---
+++ 09-modules-2/ec2/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "test" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids=["sg-01c7b204ed4d4544b"]
+
+  tags = {
+    Name = "test"
+  }
+}
