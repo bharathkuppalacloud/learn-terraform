997fe267be63a8b6d2c8242eb4704cb48d7746fe
bharathkuppala <bharath.kuppala@simplifyvms.com>
2024-12-05 15:51:09 +0530

    changed region

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,5 +1,9 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+
 resource "aws_instance" "frontend" {
-  region = "us-east-1"
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
   vpc_security_group_ids=["sg-01c7b204ed4d4544b"]
